a229f7beec87ae8d0c0b1441bb832e836ba8314c
KGY\shgkd <shgkdc@gmail.com>
2024-01-31 22:13:20 +0900

    ch.15 Service 공부 - 2

--- ch15_service/src/main/java/com/example/ch15_service/MainActivity.kt
+++ ch15_service/src/main/java/com/example/ch15_service/MainActivity.kt
@@ -1,46 +1,32 @@
 package com.example.ch15_service
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.ch15_service.ui.theme.AndroidLabTheme
+import com.example.ch15_service.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            AndroidLabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val connection: ServiceConnection = object : ServiceConnection{
+            override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+                val serviceBinder = service as MyService.MyBinder
             }
+            override fun onServiceDisconnected(name: ComponentName?) { }
         }
+
+        val intent = Intent(this, MyService::class.java)
+        bindService(intent, connection, Context.BIND_AUTO_CREATE)
+        unbindService(connection)
+
+
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidLabTheme {
-        Greeting("Android")
-    }
-}
